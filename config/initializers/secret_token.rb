6d18a6cbc5b8d5c5d6cf4098f4b95097e4832e64
elogroup <rgomes.rodrigo@gmail.com>
2012-07-13 18:24:45 -0300

    iniciando o projeto twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '471277e344936b77304809977732e1cf877a219adb2fd2ffac8d2f77a438af561467496ec1c1b2a8dc6e4b566e9b051cffabfe3a343c32e830ecf7acc6555d26'
